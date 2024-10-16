61c82c2f803f210d2db6c8793480e7e14c77a9ab
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-10-16 23:27:31 +0200

    added form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,44 +1,31 @@
-import { useEffect } from 'react';
-// import scss from './Form.module.scss';
+import { useLayoutEffect } from 'react';
+import scss from './Form.module.scss';
 
 const Form = () => {
-	useEffect(() => {
+	useLayoutEffect(() => {
+		(function (w: Window, d: Document, e: string, u: string, f: string, l?: HTMLScriptElement, n?: Element | null) {
+			(w as any)[f] =
+				(w as any)[f] ||
+				function (...args: unknown[]) {
+					(w as any)[f].q = (w as any)[f].q || [];
+					(w as any)[f].q.push(args);
+				};
 
-		const script = document.createElement('script');
-		script.src = 'https://assets.mailerlite.com/js/universal.js';
-		script.async = true;
-
-		document.body.appendChild(script);
-
-		return () => {
-			document.body.removeChild(script);
-		};
+			l = d.createElement(e) as HTMLScriptElement;
+			l.async = true;
+			l.src = u;
+			n = d.getElementsByTagName(e)[0];
+			if (n && n.parentNode) {
+				n.parentNode.insertBefore(l, n);
+			}
+		})(window, document, 'script', 'https://assets.mailerlite.com/js/universal.js', 'ml');
+		(window as any).ml('account', '1144649');
 	}, []);
 
 	return (
-		<div className='ml-embedded' data-form='gVUj3w'>
-			dupa
+		<div className={scss.form}>
+			<div className='ml-embedded form' data-form='gVUj3w'></div>
 		</div>
-		// <form className={scss.form}>
-		// 	<h2>Zapisz się do newslettera!</h2>
-		// 	<label htmlFor='name'>Imię</label>
-		// 	<input
-		// 		type='text'
-		// 		name='imię'
-		// 		pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
-		// 		title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-		// 		required={true}
-		// 	/>
-		// 	<label htmlFor='email'>Email</label>
-		// 	<input type='email' name='email' required={true} />
-		// 	<label htmlFor='phone'>Telefon</label>
-		// 	<input type='tel' name='phone' pattern='^\+?[0-9]{9,15}$' title='Wprowadź prawidłowy numer telefonu. Przykład: +48123456789' required={true} />
-		// 	<div>
-		// 		<input type='checkbox' name='privacyPolicy' required={true} />
-		// 		<label htmlFor='privacyPolicy'>Akceptuję politykę prywatności i regulamin newslettera.</label>
-		// 	</div>
-		// 	<button type='submit'>Zapisz się</button>
-		// </form>
 	);
 };
 
