ceab2786708d9841a77f9d35b63b78e7953cacfb
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-10-20 22:00:48 +0200

    media

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,10 +5,22 @@ const Footer = () => {
 		<div className={scss.footer}>
 			<h3>Skontaktuj się ze mną:</h3>
 			<ul className={scss.footer__list}>
-				<li>Linkedin</li>
-				<li>Instagram</li>
-				<li>Telefon</li>
-				<li>Mail</li>
+				<li>
+					<a href='tel:+48507656222'>507656222</a>
+				</li>
+				<li>
+					<a href='mailto:tymikk@gmail.com'>tymikk@gmail.com</a>
+				</li>
+				<li>
+					<a href='https://www.linkedin.com/in/jantyminski/' target='_blank' rel='noopener noreferrer'>
+						LinkedIn
+					</a>
+				</li>
+				<li>
+					<a href='https://www.instagram.com/diving.devops/' target='_blank' rel='noopener noreferrer'>
+						Instagram
+					</a>
+				</li>
 			</ul>
 		</div>
 	);
